2ae3e796d987e60b4a292e841e062ea8489e4e23
vanndebush <bevannstivens7@gmail.com>
2026-06-02 15:38:56 +0700

    Create mock database and setup GET /api/posts endpoint

--- server.js
+++ server.js
@@ -3,10 +3,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+let posts = [
+  {
+    id: 1,
+    author: 'vanndebush',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    createdAt: new Date().toISOString()
+  },
+  {
+    id: 2,
+    author: 'loverrukk',
+    content: 'Lorem ipsum! Dolor sit amet, consectetur adipisicing elit :(',
+    createdAt: new Date().toISOString()
+  }
+];
+
 app.get('/', (req, res) => {
   res.json({
     status: 'success',
-    message: 'DevTalk API v1.0 is Live!'
+    message: 'Welcome to DevTalk API!'
+  });
+});
+app.get('/api/posts', (req, res) => {
+  res.json({
+    status: 'success',
+    totalData: posts.length,
+    data: posts
   });
 });
 
