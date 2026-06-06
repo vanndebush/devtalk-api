52d80b2aa6671defcdfeea2f354fa1a052a160d6
vanndebush <bevannstivens7@gmail.com>
2026-06-06 14:49:15 +0700

    Setup CORS to allow cross-origin requests

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const postRoutes = require('./routes/postRoutes');
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.json({
